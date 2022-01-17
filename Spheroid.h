30f3adf32a379994ba3468f372e1f7ac75351c08
Laszlo Ashin <laszlo@ashin.hu>
2022-01-17 17:31:56 +0100

    Simplify code

--- Spheroid.h
+++ Spheroid.h
@@ -11,10 +11,6 @@ class Spheroid final : public BaseObject {
 	float ry;
 	float rxz2;
 	float ry2;
-	float ry4;
-	float rxz2ry2;
-	float rxz2ry4;
-	float rxz4ry2;
 
 public:
 	Spheroid(const Vec3f &p, int material, float rxz, float ry)
@@ -23,10 +19,6 @@ public:
 	, ry{ry}
 	, rxz2{rxz * rxz}
 	, ry2{ry * ry}
-	, ry4{ry2 * ry2}
-	, rxz2ry2{rxz2 * ry2}
-	, rxz2ry4{rxz2ry2 * ry2}
-	, rxz4ry2{rxz2ry2 * rxz2}
 	{
 		assert(fabs(rxz) > EPSILON);
 		assert(fabs(ry) > EPSILON);
@@ -38,18 +30,18 @@ public:
 		const float m1 = d.x * s.y - d.y * s.x;
 		const float m2 = d.x * s.z - d.z * s.x;
 		const float m3 = d.y * s.z - d.z * s.y;
-		const float Dlhs = rxz2ry4 * (d.x * d.x + d.z * d.z) + rxz4ry2 * d.y * d.y;
-		const float Drhs = ry4 * m2 * m2 + rxz2ry2 * (m1 * m1 + m3 * m3);
+		const float Dlhs = rxz2 * (ry2 * (d.x * d.x + d.z * d.z) + rxz2 * d.y * d.y);
+		const float Drhs = ry2 * m2 * m2 + rxz2 * (m1 * m1 + m3 * m3);
 		if (Dlhs < Drhs) return std::make_tuple(-1.0f, Vec3f{});
 
-		const float Dsqrt = sqrtf(Dlhs - Drhs);
-		const float a = (d.x * d.x + d.z * d.z) * ry2 + d.y * d.y * rxz2;
+		const float Dsqrt = sqrtf(ry2 * (Dlhs - Drhs));
 		const float minusb = -((s.x * d.x + s.z * d.z) * ry2 + s.y * d.y * rxz2);
 		float t1 = minusb + Dsqrt;
 		if (t1 < 0.0f) return std::make_tuple(-1.0f, Vec3f{});
 		float t0 = minusb - Dsqrt;
 		if (t0 < 0.0f) t0 = t1;
 
+		const float a = (d.x * d.x + d.z * d.z) * ry2 + d.y * d.y * rxz2;
 		t0 /= a;
 
 		const Vec3f mp = r.s + r.d * t0;
