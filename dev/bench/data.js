33031dc68c1d43aad645600bd7096e36b32868d3
github-action-benchmark <github@users.noreply.github.com>
2022-01-18 15:09:55 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 55d0d142a3f3b15343055d9ad7c5ce1471fd4b85

---
+++ dev/bench/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1642518595399,
+  "repoUrl": "https://github.com/LaszloAshin/ray",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "laszlo@ashin.hu",
+            "name": "Laszlo Ashin",
+            "username": "LaszloAshin"
+          },
+          "committer": {
+            "email": "laszlo@ashin.hu",
+            "name": "Laszlo Ashin",
+            "username": "LaszloAshin"
+          },
+          "distinct": true,
+          "id": "55d0d142a3f3b15343055d9ad7c5ce1471fd4b85",
+          "message": "Enable benchmarks",
+          "timestamp": "2022-01-18T16:04:57+01:00",
+          "tree_id": "625949b23651648dedc6cc946d518302a6bbd6f0",
+          "url": "https://github.com/LaszloAshin/ray/commit/55d0d142a3f3b15343055d9ad7c5ce1471fd4b85"
+        },
+        "date": 1642518593876,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Benchmark - Executable Size",
+            "value": 30936,
+            "unit": "Bytes"
+          }
+        ]
+      }
+    ]
+  }
+}
