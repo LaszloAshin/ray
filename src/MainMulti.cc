8e78c4a5ec63f5c013d338d54b43d7ae67bc850b
Laszlo Ashin <laszlo@ashin.hu>
2022-01-18 23:01:10 +0100

    Merge MultithreadedTracer into MainMulti

--- src/MainMulti.cc
+++ src/MainMulti.cc
@@ -1,11 +1,40 @@
-#include "Targa.h"
 #include "Scene.h"
-#include "MultithreadedTracer.h"
+#include "Targa.h"
+#include "Tracer.h"
+#include "Vector.h"
 
 #include "config.h"
 
 #include <cstdio>
 #include <cstdlib> // atoi()
+#include <mutex>
+#include <thread>
+
+struct MultithreadedTracer : public Tracer {
+	using Tracer::Tracer;
+
+private:
+	int getNextBlock() override {
+		std::unique_lock<std::mutex> lk{mutex_block};
+		return Tracer::getNextBlock();
+	}
+
+	void consumeBlocks(bool turbo) override {
+		const int nthreads = std::thread::hardware_concurrency();
+		Vector<std::thread, 32> threads;
+
+		fprintf(stderr, "Spawning %d threads...\n", nthreads);
+		for (int i = 0; i < nthreads; ++i) {
+			threads.emplace_back([this, turbo]{ Tracer::consumeBlocks(turbo); });
+		}
+
+		for (int i = 0; i < nthreads; ++i) {
+			threads[i].join();
+		}
+	}
+
+	std::mutex mutex_block;
+};
 
 int
 main(int argc, char *argv[])
