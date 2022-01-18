734aeb8fbf29e913760dd3d977241f1b5fdb4f06
github-action-benchmark <github@users.noreply.github.com>
2022-01-18 15:28:09 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 244b13ffa0ad4143d79c891245f8ceff268c0214

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1642518595399,
+  "lastUpdate": 1642519689750,
   "repoUrl": "https://github.com/LaszloAshin/ray",
   "entries": {
     "Benchmark": [
@@ -23,6 +23,35 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/LaszloAshin/ray/commit/55d0d142a3f3b15343055d9ad7c5ce1471fd4b85"
         },
         "date": 1642518593876,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Benchmark - Executable Size",
+            "value": 30936,
+            "unit": "Bytes"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "laszlo@ashin.hu",
+            "name": "Laszlo Ashin",
+            "username": "LaszloAshin"
+          },
+          "committer": {
+            "email": "laszlo@ashin.hu",
+            "name": "Laszlo Ashin",
+            "username": "LaszloAshin"
+          },
+          "distinct": true,
+          "id": "244b13ffa0ad4143d79c891245f8ceff268c0214",
+          "message": "Build single and multithreaded executables",
+          "timestamp": "2022-01-18T16:27:01+01:00",
+          "tree_id": "52c6e4c03a7c04ed93ac0ea84753a14629d74d41",
+          "url": "https://github.com/LaszloAshin/ray/commit/244b13ffa0ad4143d79c891245f8ceff268c0214"
+        },
+        "date": 1642519688783,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
