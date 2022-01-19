8b70f678ca873e6de51a373c921f50b9ffebdf7b
github-action-benchmark <github@users.noreply.github.com>
2022-01-19 21:33:22 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for ea7c8de02ea21d76325e641becd138e1af938a93

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1642626188740,
+  "lastUpdate": 1642628002045,
   "repoUrl": "https://github.com/LaszloAshin/ray",
   "entries": {
     "Benchmark": [
@@ -597,6 +597,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Benchmark - CPU Cycles",
             "value": 517014143,
+            "unit": "Cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "laszlo@ashin.hu",
+            "name": "Laszlo Ashin",
+            "username": "LaszloAshin"
+          },
+          "committer": {
+            "email": "laszlo@ashin.hu",
+            "name": "Laszlo Ashin",
+            "username": "LaszloAshin"
+          },
+          "distinct": true,
+          "id": "ea7c8de02ea21d76325e641becd138e1af938a93",
+          "message": "Remove unneeded executable sections",
+          "timestamp": "2022-01-19T22:32:12+01:00",
+          "tree_id": "1c2f94ca7b5adf10a68237878e3aa23b15dd54b4",
+          "url": "https://github.com/LaszloAshin/ray/commit/ea7c8de02ea21d76325e641becd138e1af938a93"
+        },
+        "date": 1642628001196,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Benchmark - Executable Size",
+            "value": 30936,
+            "unit": "Bytes"
+          },
+          {
+            "name": "Benchmark - text Size",
+            "value": 16695,
+            "unit": "Bytes"
+          },
+          {
+            "name": "Benchmark - data Size",
+            "value": 832,
+            "unit": "Bytes"
+          },
+          {
+            "name": "Benchmark - bss Size",
+            "value": 16,
+            "unit": "Bytes"
+          },
+          {
+            "name": "Benchmark - CPU Cycles",
+            "value": 517014062,
             "unit": "Cycles"
           }
         ]
