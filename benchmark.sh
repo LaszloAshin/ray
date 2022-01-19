23c57c3fa85a9195ea99fbeb5ebde6d4f3e8fbab
Laszlo Ashin <laszlo@ashin.hu>
2022-01-19 22:01:23 +0100

    Measure sections of the executable

--- benchmark.sh
+++ benchmark.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -uex
 
 strip --strip-all ray
+eval $(size ray-single | awk '/ray-single/ { print "export TEXT=" $1 " DATA=" $2 " BSS=" $3 }')
 
 export WIDTH=64 HEIGHT=64
 CYCLES=$(valgrind --tool=cachegrind ./ray-single 2>&1 | awk '/I   refs:/ { gsub(",", "", $4); print $4 }')
@@ -11,6 +12,21 @@ cat << EOF
         "name": "Benchmark - Executable Size",
         "unit": "Bytes",
         "value": $(stat --format=%s ray)
+    },
+    {
+        "name": "Benchmark - text Size",
+        "unit": "Bytes",
+        "value": ${TEXT}
+    },
+    {
+        "name": "Benchmark - data Size",
+        "unit": "Bytes",
+        "value": ${DATA}
+    },
+    {
+        "name": "Benchmark - bss Size",
+        "unit": "Bytes",
+        "value": ${BSS}
     },
     {
         "name": "Benchmark - CPU Cycles",
