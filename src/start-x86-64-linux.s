b7e56532fc591973de484807b512c70b5e869ffc
Laszlo Ashin <laszlo@ashin.hu>
2022-01-21 17:42:01 +0100

    Add -nostartfiles on Linux

---
+++ src/start-x86-64-linux.s
@@ -0,0 +1,17 @@
+	.text
+	.globl _start
+	.type _start,@function
+
+# _start stack:
+# argc  %rsp + 0
+# argv  %rsp + 8
+# envp  %rsp + 8 + argc * 8
+_start:
+	pop    %rdi # argc
+	mov    %rsp, %rsi # argv
+	and    $0xfffffffffffffff0, %rsp # align stack
+	callq  main # x86_64 ABI function params: %rdi %rsi %rdx %rcx %r8 %r9
+	mov    %rax, %rdi # exit status
+	xor    %rax, %rax
+	mov    $60, %al # syscall number
+	syscall
