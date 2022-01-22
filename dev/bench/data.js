ce587529aa6a546a7b060cfd26f395d84ac2ed9e
github-action-benchmark <github@users.noreply.github.com>
2022-01-22 11:59:23 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 27bf5383b65bdfa6e02db942fdbbb5ca6d4664a9

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1642849997184,
+  "lastUpdate": 1642852763817,
   "repoUrl": "https://github.com/LaszloAshin/ray",
   "entries": {
     "Benchmark": [
@@ -1430,6 +1430,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Benchmark - CPU Cycles",
             "value": 488064233,
+            "unit": "Cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "laszlo@ashin.hu",
+            "name": "Laszlo Ashin",
+            "username": "LaszloAshin"
+          },
+          "committer": {
+            "email": "laszlo@ashin.hu",
+            "name": "Laszlo Ashin",
+            "username": "LaszloAshin"
+          },
+          "distinct": true,
+          "id": "27bf5383b65bdfa6e02db942fdbbb5ca6d4664a9",
+          "message": "Simplify CMakeLists.txt",
+          "timestamp": "2022-01-22T12:58:28+01:00",
+          "tree_id": "1ee7e661580ccb804a6ea08cd875104c52180a19",
+          "url": "https://github.com/LaszloAshin/ray/commit/27bf5383b65bdfa6e02db942fdbbb5ca6d4664a9"
+        },
+        "date": 1642852763102,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Benchmark - Executable Size",
+            "value": 17736,
+            "unit": "Bytes"
+          },
+          {
+            "name": "Benchmark - text Size",
+            "value": 11685,
+            "unit": "Bytes"
+          },
+          {
+            "name": "Benchmark - data Size",
+            "value": 648,
+            "unit": "Bytes"
+          },
+          {
+            "name": "Benchmark - bss Size",
+            "value": 0,
+            "unit": "Bytes"
+          },
+          {
+            "name": "Benchmark - CPU Cycles",
+            "value": 488064215,
             "unit": "Cycles"
           }
         ]
