932e303e371c8abc17f08aced82b37b1c6419a1b
github-action-benchmark <github@users.noreply.github.com>
2022-01-23 11:13:25 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 7a18a584a884311ea8547a77c0f2caa0c0fd4d00

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1642864623210,
+  "lastUpdate": 1642936405937,
   "repoUrl": "https://github.com/LaszloAshin/ray",
   "entries": {
     "Benchmark": [
@@ -1528,6 +1528,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Benchmark - CPU Cycles",
             "value": 488064215,
+            "unit": "Cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "laszlo@ashin.hu",
+            "name": "Laszlo Ashin",
+            "username": "LaszloAshin"
+          },
+          "committer": {
+            "email": "laszlo@ashin.hu",
+            "name": "Laszlo Ashin",
+            "username": "LaszloAshin"
+          },
+          "distinct": true,
+          "id": "7a18a584a884311ea8547a77c0f2caa0c0fd4d00",
+          "message": "Use sincosf instead of separate sinf and cosf",
+          "timestamp": "2022-01-23T12:11:51+01:00",
+          "tree_id": "2611cb91fa6fb05d64978631c4dc812106562880",
+          "url": "https://github.com/LaszloAshin/ray/commit/7a18a584a884311ea8547a77c0f2caa0c0fd4d00"
+        },
+        "date": 1642936405498,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Benchmark - Executable Size",
+            "value": 17736,
+            "unit": "Bytes"
+          },
+          {
+            "name": "Benchmark - text Size",
+            "value": 11237,
+            "unit": "Bytes"
+          },
+          {
+            "name": "Benchmark - data Size",
+            "value": 648,
+            "unit": "Bytes"
+          },
+          {
+            "name": "Benchmark - bss Size",
+            "value": 0,
+            "unit": "Bytes"
+          },
+          {
+            "name": "Benchmark - CPU Cycles",
+            "value": 488064588,
             "unit": "Cycles"
           }
         ]
