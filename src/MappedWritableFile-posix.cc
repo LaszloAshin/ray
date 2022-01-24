375f18d513fdc15d3461052b2db993519122ebfd
Laszlo Ashin <laszlo@ashin.hu>
2022-01-24 21:31:40 +0100

    Get rid of puts
    gcc is smart enough to replace all instances of printf where
    there is no format argument and the format string ends in a newline.
    This is not good for us because puts means another imported symbol
    from libc. We are on our way to get rid all of the imports.

--- src/MappedWritableFile-posix.cc
+++ src/MappedWritableFile-posix.cc
@@ -11,16 +11,16 @@ MappedWritableFile::MappedWritableFile(const char* fname, int length)
 {
 	const int fd = open(fname, O_CREAT | O_RDWR, 0644);
 	if (fd == -1) {
-		printf("Failed to open output file\n");
+		printf("Failed to open %s\n", fname);
 		_exit(1);
 	}
 	if (ftruncate(fd, length)) {
-		printf("Failed to allocate output file\n");
+		printf("Failed to allocate %s\n", fname);
 		_exit(1);
 	}
 	address_ = static_cast<uint8_t*>(mmap(NULL, length, PROT_WRITE, MAP_SHARED, fd, 0));
 	if (address_ == MAP_FAILED) {
-		printf("Failed to map output file\n");
+		printf("Failed to map %s\n", fname);
 		_exit(1);
 	}
 	close(fd);
@@ -28,6 +28,6 @@ MappedWritableFile::MappedWritableFile(const char* fname, int length)
 
 MappedWritableFile::~MappedWritableFile() {
 	if (munmap(address_, length_)) {
-		printf("Failed to unmap output file\n");
+		printf("Failed to unmap output file at %p\n", address_);
 	}
 }
