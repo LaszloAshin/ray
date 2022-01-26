0f6a811f66ea63ab5a15434bb91169646f2bfc5c
Laszlo Ashin <laszlo@ashin.hu>
2022-01-26 16:01:03 +0100

    Implement syscall mmap on linux and clean up asm

--- src/MappedWritableFile-posix.cc
+++ src/MappedWritableFile-posix.cc
@@ -12,8 +12,8 @@
 #include <sys/syscall.h>
 
 inline void myexit(int status) {
-	__asm __volatile__(
-		"syscall;"
+	__asm __volatile__("\n\t"
+		"syscall\n\t"
 		:
 		: "a"((long)SYS_exit), "D"(status)
 		: "%rcx", "%r11", "memory"
@@ -22,8 +22,8 @@ inline void myexit(int status) {
 
 inline int myclose(int fd) {
 	int result;
-	__asm __volatile__(
-		"syscall;"
+	__asm __volatile__("\n\t"
+		"syscall\n\t"
 		: "=a"(result)
 		: "0"((long)SYS_close), "D"(fd)
 		: "%rcx", "%r11", "memory"
@@ -33,8 +33,8 @@ inline int myclose(int fd) {
 
 inline int myftruncate(int fd, long length) {
 	int result;
-	__asm __volatile__(
-		"syscall;"
+	__asm __volatile__("\n\t"
+		"syscall\n\t"
 		: "=a"(result)
 		: "0"((long)SYS_ftruncate), "D"(fd), "S"(length)
 		: "%rcx", "%r11", "memory"
@@ -44,8 +44,8 @@ inline int myftruncate(int fd, long length) {
 
 inline int myopen(const char* fname, int flags, unsigned mode) {
 	int result;
-	__asm __volatile__(
-		"syscall;"
+	__asm __volatile__("\n\t"
+		"syscall\n\t"
 		: "=a"(result)
 		: "0"((long)SYS_open), "D"(fname), "S"(flags), "d"(mode)
 		: "%rcx", "%r11", "memory"
@@ -55,11 +55,25 @@ inline int myopen(const char* fname, int flags, unsigned mode) {
 
 inline int mymunmap(void* addr, long length) {
 	int result;
-	__asm __volatile__(
-		"syscall;"
+	__asm __volatile__("\n\t"
+		"syscall\n\t"
 		: "=a"(result)
 		: "0"((long)SYS_munmap), "D"(addr), "S"(length)
 		: "%rcx", "%r11", "memory"
+	);
+	return result;
+}
+
+inline void* mymmap(void* addr, long length, int prot, int flags, int fd, long offset) {
+	void* result;
+	__asm __volatile__("\n\t"
+		"movl %5, %%r10d\n\t"
+		"movl %6, %%r8d\n\t"
+		"movq %7, %%r9\n\t"
+		"syscall\n\t"
+		: "=a"(result)
+		: "0"((long)SYS_mmap), "D"(addr), "S"(length), "d"(prot), "r"(flags), "r"(fd), "r"(offset)
+		: "%rcx", "%r11", "memory", "%r10", "%r8", "%r9"
 	);
 	return result;
 }
@@ -71,6 +85,7 @@ inline int mymunmap(void* addr, long length) {
 #define myftruncate ftruncate
 #define myopen open
 #define mymunmap munmap
+#define mymmap mmap
 
 #endif
 
@@ -86,8 +101,8 @@ MappedWritableFile::MappedWritableFile(const char* fname, int length)
 		myprint("Fail: ftruncate\n");
 		myexit(1);
 	}
-	address_ = static_cast<uint8_t*>(mmap(NULL, length, PROT_WRITE, MAP_SHARED, fd, 0));
-	if (address_ == MAP_FAILED) {
+	address_ = static_cast<uint8_t*>(mymmap(NULL, length, PROT_WRITE, MAP_SHARED, fd, 0));
+	if (reinterpret_cast<unsigned long>(address_) > -4096UL) {
 		myprint("Fail: mmap\n");
 		myexit(1);
 	}
--- src/MyMath-inlineasm.cc
+++ src/MyMath-inlineasm.cc
@@ -5,21 +5,21 @@ void mysincosf(float x, float* sinx, float* cosx) {
 	long double cosr;
 	long double sinr;
 	unsigned int swtmp;
-	__asm __volatile__(
-		"fsincos;"
-		"fnstsw %w2;"
-		"testl $0x400, %2;"
-		"jz 1f;"
-		"fldpi;"
-		"fadd %%st(0);"
-		"fxch %%st(1);"
-		"2: fprem1;"
-		"fnstsw %w2;"
-		"testl $0x400, %2;"
-		"jnz 2b;"
-		"fstp %%st(1);"
-		"fsincos;"
-		"1:"
+	__asm __volatile__("\n\t"
+		"fsincos\n\t"
+		"fnstsw %w2\n\t"
+		"testl $0x400, %2\n\t"
+		"jz 1f\n\t"
+		"fldpi\n\t"
+		"fadd %%st(0)\n\t"
+		"fxch %%st(1)\n\t"
+		"2: fprem1\n\t"
+		"fnstsw %w2\n\t"
+		"testl $0x400, %2\n\t"
+		"jnz 2b\n\t"
+		"fstp %%st(1)\n\t"
+		"fsincos\n\t"
+		"1:\n\t"
 		: "=t" (cosr), "=u" (sinr), "=a" (swtmp)
 		: "0" (x)
 	);
@@ -29,8 +29,8 @@ void mysincosf(float x, float* sinx, float* cosx) {
 
 float myatan2f(float y, float x) {
 	long double result;
-	__asm __volatile__(
-		"fpatan"
+	__asm __volatile__("\n\t"
+		"fpatan\n\t"
 		: "=t" (result)
 		: "0" (x), "u" (y)
 		: "st(1)"
