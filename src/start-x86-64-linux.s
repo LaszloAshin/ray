a21b1af2940e2757e6bc386cc34fcc6c2cba7ca8
Laszlo Ashin <laszlo@ashin.hu>
2022-01-31 11:36:45 +0100

    Move _start to front

--- src/start-x86-64-linux.s
+++ src/start-x86-64-linux.s
@@ -1,6 +1,7 @@
 	.text
 	.globl _start
 	.type _start,@function
+	.section .text.exit
 
 # _start stack:
 # argc  %rsp + 0
@@ -11,7 +12,7 @@ _start:
 	mov    %rsp, %rsi # argv
 	lea    8(%rsi, %rdi, 8), %rdx # envp
 	and    $-16, %rsp # align stack
-	callq  main # x86_64 ABI function params: %rdi %rsi %rdx %rcx %r8 %r9
+	call   main # x86_64 ABI function params: %rdi %rsi %rdx %rcx %r8 %r9
 	mov    %eax, %edi # exit status
 	xor    %eax, %eax
 	mov    $60, %al # syscall number
