252cf08812b379c15f30e886465abf6c3dec1c50
Laszlo Ashin <laszlo@ashin.hu>
2022-02-04 12:19:18 +0100

    Remove dependencies of ray.exe

--- src/start-windows.c
+++ src/start-windows.c
@@ -1,8 +1,8 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
-int main();
+int main(int argc, char* argv[], char* envp[]);
 
 int mainCRTStartup() {
-       ExitProcess(main());
+       ExitProcess(main(0, NULL, NULL));
 }
