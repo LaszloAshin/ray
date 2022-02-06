d04cb798e0caf37a68c0fac1304a4f7e7b744a03
github-action-benchmark <github@users.noreply.github.com>
2022-02-06 23:08:57 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 1a70bafb16477315d6a66e52d094a9bff122ff4a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1643990643752,
+  "lastUpdate": 1644188937130,
   "repoUrl": "https://github.com/LaszloAshin/ray",
   "entries": {
     "Benchmark": [
@@ -5815,6 +5815,55 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/LaszloAshin/ray/commit/412f7c722f080bd43d393d545aed82d03d94a3d9"
         },
         "date": 1643990642752,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Benchmark - Executable Size",
+            "value": 7700,
+            "unit": "Bytes"
+          },
+          {
+            "name": "Benchmark - text Size",
+            "value": 6889,
+            "unit": "Bytes"
+          },
+          {
+            "name": "Benchmark - data Size",
+            "value": 0,
+            "unit": "Bytes"
+          },
+          {
+            "name": "Benchmark - bss Size",
+            "value": 0,
+            "unit": "Bytes"
+          },
+          {
+            "name": "Benchmark - CPU Cycles",
+            "value": 971380678,
+            "unit": "Cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "laszlo@ashin.hu",
+            "name": "Laszlo Ashin",
+            "username": "LaszloAshin"
+          },
+          "committer": {
+            "email": "laszlo@ashin.hu",
+            "name": "Laszlo Ashin",
+            "username": "LaszloAshin"
+          },
+          "distinct": true,
+          "id": "1a70bafb16477315d6a66e52d094a9bff122ff4a",
+          "message": "Shave off some bytes in asm",
+          "timestamp": "2022-02-07T00:07:05+01:00",
+          "tree_id": "0417321d4d09ebbc312b5d94220add5a0d5ded5a",
+          "url": "https://github.com/LaszloAshin/ray/commit/1a70bafb16477315d6a66e52d094a9bff122ff4a"
+        },
+        "date": 1644188936254,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
