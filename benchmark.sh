3377427265dafb74139b29fb6976f03e3cb1ec89
Laszlo Ashin <laszlo@ashin.hu>
2022-02-09 11:41:14 +0100

    Unify pipelines and measure all platforms

--- benchmark.sh
+++ benchmark.sh
@@ -1,16 +1,31 @@
 #!/bin/sh -uex
 
-eval $(size ray-single | awk '/ray-single/ { print "export TEXT=" $1 " DATA=" $2 " BSS=" $3 }')
+find .
+U=artifact-ubuntu-latest
+W=artifact-windows-latest
+M=artifact-macos-latest
+
+eval $(size $U/ray-single | awk '/ray-single/ { print "export TEXT=" $1 " DATA=" $2 " BSS=" $3 }')
 
 export WIDTH=64 HEIGHT=64
-CYCLES=$(valgrind --tool=cachegrind ./ray-single 2>&1 | awk '/I   refs:/ { gsub(",", "", $4); print $4 }')
+CYCLES=$(valgrind --tool=cachegrind $U/ray-single 2>&1 | awk '/I   refs:/ { gsub(",", "", $4); print $4 }')
 
 cat << EOF
 [
     {
         "name": "Benchmark - Executable Size",
         "unit": "Bytes",
-        "value": $(stat --format=%s ray.vndh)
+        "value": $(stat --format=%s $U/ray.vndh)
+    },
+    {
+        "name": "Benchmark - Win32 Executable Size",
+        "unit": "Bytes",
+        "value": $(stat --format=%s $W/ray.exe)
+    },
+    {
+        "name": "Benchmark - MacOS Executable Size",
+        "unit": "Bytes",
+        "value": $(stat --format=%s $M/ray)
     },
     {
         "name": "Benchmark - text Size",
