0f7d920dcb840abb6f93d632b49560b87291cadd
Laszlo Ashin <laszlo@ashin.hu>
2022-02-18 16:48:55 +0100

    Compact macho executables

---
+++ src/compact-macho.c
@@ -0,0 +1,116 @@
+#include <mach-o/loader.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+const char* name_load_command(uint32_t cmd) {
+	switch (cmd) {
+	case LC_SYMTAB: return "LC_SYMTAB";
+	case LC_UNIXTHREAD: return "LC_UNIXTHREAD";
+	case LC_SEGMENT_64: return "LC_SEGMENT_64";
+	case LC_SOURCE_VERSION: return "LC_SOURCE_VERSION";
+	}
+	return "UNKNOWN";
+}
+
+struct thread_command_unixthread64 {
+	uint32_t cmd;
+	uint32_t cmdsize;
+	uint32_t flavor;
+	uint32_t count;
+	x86_thread_state64_t state;
+};
+
+int main(int argc, char* argv[]) {
+	(void)argc;
+	int v = !strcmp(argv[1], "-v");
+	v && ++argv;
+	FILE* fp = fopen(argv[1], "rb");
+
+	struct mach_header_64 hd;
+	fread(&hd, sizeof(struct mach_header_64), 1, fp);
+	v && printf("number of load commands: %d\n", hd.ncmds);
+	v && printf("size of load commands: %d\n", hd.sizeofcmds);
+
+
+	char* p = (char*)malloc(hd.sizeofcmds);
+	fread(p, hd.sizeofcmds, 1, fp);
+	char* q = p;
+	uint64_t payload_file_offset = 0;
+	uint64_t payload_file_size = 0;
+	uint64_t payload_addr = 0;
+	uint64_t payload_entry_point = 0;
+	for (unsigned i = 0; i < hd.ncmds; ++i) {
+		struct load_command* lc = (struct load_command*)q;
+		v && printf("- load_command: %s(%d)\n", name_load_command(lc->cmd), lc->cmd);
+		v && printf("  size: %d\n", lc->cmdsize);
+
+		if (lc->cmd == LC_SEGMENT_64) {
+			struct segment_command_64* seg = (struct segment_command_64*)lc;
+			v && printf("  segment name: %s\n", seg->segname);
+			v && printf("  vmaddr: 0x%llx\n", seg->vmaddr);
+			v && printf("  vmsize: 0x%llx\n", seg->vmsize);
+			v && printf("  fileoff: 0x%llx\n", seg->fileoff);
+			v && printf("  filesize: 0x%llx\n", seg->filesize);
+			v && printf("  number of sections: %d\n", seg->nsects);
+
+			struct section_64* sect = (struct section_64*)(q + sizeof(struct segment_command_64));
+			for (unsigned j = 0; j < seg->nsects; ++j) {
+				v && printf("  - section name: %s\n", sect[j].sectname);
+				v && printf("    addr: 0x%llx\n", sect[j].addr);
+				v && printf("    size: 0x%llx\n", sect[j].size);
+				v && printf("    offset: 0x%x\n", sect[j].offset);
+			}
+
+			if (!strcmp(seg->segname, "__TEXT")) {
+				payload_file_offset = sect[0].offset;
+				payload_file_size = sect[seg->nsects-1].offset + sect[seg->nsects-1].size - sect[0].offset;
+				payload_addr = sect[0].addr;
+			}
+		} else if (lc->cmd == LC_UNIXTHREAD) {
+			struct thread_command_unixthread64* tc = (struct thread_command_unixthread64*)lc;
+			v && printf("  rip: 0x%llx\n", tc->state.__rip);
+			payload_entry_point = tc->state.__rip;
+		}
+		q += lc->cmdsize;
+	}
+
+	free(p);
+
+	v && printf("payload_file_offset = 0x%llx\n", payload_file_offset);
+	v && printf("payload_file_size = 0x%llx\n", payload_file_size);
+
+	p = (char*)malloc(payload_file_size);
+	fseek(fp, payload_file_offset, SEEK_SET);
+	fread(p, payload_file_size, 1, fp);
+	fclose(fp);
+
+	char payload_filename[64];
+	snprintf(payload_filename, 64, "%s.payload", argv[1]);
+
+	fp = fopen(payload_filename, "wb");
+	fwrite(p, payload_file_size, 1, fp);
+	fclose(fp);
+
+	free(p);
+
+	char arg_payload_addr[64];
+	snprintf(arg_payload_addr, 64, "-DPAYLOAD_ADDR=0x%llx", payload_addr);
+	v && puts(arg_payload_addr);
+
+	char arg_payload_entry_point[64];
+	snprintf(arg_payload_entry_point, 64, "-DPAYLOAD_ENTRY_POINT=0x%llx", payload_entry_point);
+	v && puts(arg_payload_entry_point);
+
+	char arg_payload_filename[64];
+	snprintf(arg_payload_filename, 64, "-DPAYLOAD_FILENAME=\"%s\"", payload_filename);
+	v && puts(arg_payload_filename);
+
+	char arg_output_filename[64];
+	snprintf(arg_output_filename, 64, "-o%s.compact", argv[1]);
+	v && puts(arg_output_filename);
+
+	execlp("nasm", "nasm", arg_payload_addr, arg_payload_entry_point, arg_payload_filename, arg_output_filename, "../src/compact-macho.s", NULL);
+	perror("execlp");
+}
---
+++ src/compact-macho.s
@@ -0,0 +1,100 @@
+%define MH_MAGIC_64                 0xfeedfacf
+%define CPU_ARCH_ABI64              0x01000000
+%define CPU_TYPE_I386               0x00000007
+%define CPU_TYPE_X86_64             CPU_ARCH_ABI64 | CPU_TYPE_I386
+%define CPU_SUBTYPE_LIB64           0x80000000
+%define CPU_SUBTYPE_I386_ALL        0x00000003
+%define MH_EXECUTE                  0x2
+%define MH_NOUNDEFS                 0x1
+%define LC_SEGMENT_64               0x19
+%define LC_UNIXTHREAD               0x5
+%define VM_PROT_READ                0x1
+%define VM_PROT_WRITE               0x2
+%define VM_PROT_EXECUTE             0x4
+%define VM_PROT_RX                  VM_PROT_READ | VM_PROT_EXECUTE
+%define VM_PROT_RWX                 VM_PROT_READ | VM_PROT_WRITE | VM_PROT_EXECUTE
+%define x86_THREAD_STATE64          0x4
+%define x86_EXCEPTION_STATE64_COUNT 42
+%define WRITABLE_TEXT_ADDR          0x100000000
+
+bits 64
+
+org 0x100010000
+
+main_header:
+	dd    MH_MAGIC_64                              ; magic
+	dd    CPU_TYPE_X86_64                          ; cputype
+	dd    CPU_SUBTYPE_LIB64 | CPU_SUBTYPE_I386_ALL ; cpusubtype
+	dd    MH_EXECUTE                               ; filetype
+	dd    4                                        ; ncmds
+	dd    commands.end  - commands +3*8            ; sizeofcmds
+	dd    MH_NOUNDEFS                              ; flags
+	dd    0                                        ; reserved
+
+commands:
+
+pagezero:
+	dd    LC_SEGMENT_64           ; cmd
+	dd    pagezero.end - pagezero ; command size
+.name:	db    '__PAGEZERO', 0
+..@code:
+	rep   movsb
+	jmp   rbx
+	times 16 - $ + .name db 0     ; segment name (pad to 16 bytes)
+	dq    0                       ; vmaddr
+	dq    WRITABLE_TEXT_ADDR      ; vmsize
+	dq    0                       ; fileoff
+	dq    0                       ; filesize
+	dd    0                       ; maxprot
+	dd    0                       ; initprot
+	dd    0                       ; nsects
+	dd    0                       ; flags
+.end:
+
+wrtext:
+	dd    LC_SEGMENT_64                    ; cmd
+	dd    wrtext.end - wrtext              ; command size
+.name:	db    '__TEXTwr', 0                    ; segment name (pad to 16 bytes)
+	times 16 - $ + .name db 0
+	dq    WRITABLE_TEXT_ADDR               ; vmaddr
+	dq    main_header - WRITABLE_TEXT_ADDR ; vmsize
+	dq    0                                ; fileoff
+	dq    0                                ; filesize
+	dd    VM_PROT_RWX                      ; maxprot
+	dd    VM_PROT_RWX                      ; initprot
+	dd    0                                ; nsects
+	dd    0                                ; flags
+.end:
+
+text:
+	dd    LC_SEGMENT_64             ; cmd
+	dd    text.end - text           ; command size
+.name:	db    '__TEXT', 0               ; segment name (pad to 16 bytes)
+	times 16 - $ + .name db 0
+	dq    main_header               ; vmaddr
+	dq    payload.end - main_header ; vmsize
+	dq    0                         ; fileoff
+	dq    payload.end - main_header ; filesize
+	dd    VM_PROT_RWX               ; maxprot
+	dd    VM_PROT_RX                ; initprot
+	dd    0                         ; nsects
+	dd    0                         ; flags
+.end:
+
+unixthread:
+	dd    LC_UNIXTHREAD                                    ; cmd
+	dd    unixthread.end - unixthread + 3*8                ; cmdsize
+	dd    x86_THREAD_STATE64                               ; flavor
+	dd    x86_EXCEPTION_STATE64_COUNT                      ; count
+	dq    0, PAYLOAD_ENTRY_POINT, payload.end - payload, 0 ; rax, rbx, rcx, rdx
+	dq    PAYLOAD_ADDR, payload, 0, 0                      ; rdi, rsi, rbp, rsp(must be zero)
+	dq    0, 0, 0, 0                                       ; r8, r9, r10, r11
+	dq    0, 0, 0, 0                                       ; r12, r13, r14, r15
+	dq    ..@code, 0;, 0, 0, 0                             ; rip, rflags(should be zero), cs, fs, gs
+.end:
+
+commands.end:
+
+payload:
+	incbin PAYLOAD_FILENAME
+.end:
