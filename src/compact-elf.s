9ec59a2843ba3798b0f2789d19315e256254d312
Laszlo Ashin <laszlo@ashin.hu>
2022-03-01 14:18:38 +0100

    Fix some repetition and shave off a byte on linux

--- src/compact-elf.s
+++ src/compact-elf.s
@@ -1,43 +1,45 @@
 bits 32
 
-org 0x08048000
+%define ORIGIN 0x08048000
+
+org ORIGIN
 
 ehdr:
 	db 0x7F,"ELF"
-start.1:
-	mov esi, payload.end - 1 ; 5
-	mov edi, PAYLOAD_ADDR + PAYLOAD_SIZE + payload.end - payload ; 5
-	jmp short start.2 ; 2
+start.2:
+	lea esi, [byte eax + ecx + payload - start.1 - 1] ; 4 ; eax=0x8048014 esi=payload.end-1
+	lea edi, [byte esi + edx + (PAYLOAD_ADDR - ORIGIN) - (payload - $$) - PAQ_OFFSET - 1] ; 4 ; edi=PAYLOAD_ADDR + PAYLOAD_SIZE + payload.end - payload
+	rep movsb ; 2
+	jmp short start.3 ; 2
 	dw 2 ; e_type (ET_EXEC)
 	dw 3 ; e_machine (EM_386)
-start.2:
+start.1:
 	std ; e_version byte 1
-	nop ; e_version byte 2
-	jmp short start.3 ; e_version byte 3 & 4
+	mov dl, (PAYLOAD_SIZE + 1 + 1 + PAQ_OFFSET) % 256 ; 2 ; e_version byte 2 & 3
+	db 0xb8 ; e_version byte 4: mov eax, ...
 	dd start.1 ; e_entry
-	dd phdr - $$ ; e_phoff
-start.3:
+	dd phdr - $$ ; e_phoff ; sub 0, al; add [eax], al
 	mov cx, payload.end - payload ; e_shoff
-	rep movsb ; e_flags byte 1 & 2
-	jmp short start.4; e_flags byte 3 & 4
+	mov dh, (PAYLOAD_SIZE + 1 + 1 + PAQ_OFFSET) / 256 ; e_flags byte 1 & 2
+	jmp short start.2; e_flags byte 3 & 4
 	dw 52 ; e_ehsize
 	dw 32 ; e_phentsize
 phdr:
 	dd 1 ; e_phnum; e_shentsize; p_type = PT_LOAD
 	dd 0 ; e_shnum; e_shstrndx; p_offset
 	dd $$ ; p_vaddr
-start.4:
+start.3:
 	inc edi ; p_addr byte 1
 	push edi ; p_addr byte 2
-	jmp edi; p_addr byte 3 & 4
+	jmp edi; p_addr byte 3 & 4; jump to relocated payload
 	dd payload.end - $$ ; p_filesz
 	dd PAYLOAD_ADDR - $$ + PAYLOAD_SIZE + payload.end - payload + 1 ; memsz XXX we should add paq_trailing_zeroes
 ;	dd 7 ; p_flags
 ;	dd 0 ; p_align
 
 payload:
-	mov cx, PAYLOAD_SIZE + 1 + 1 + PAQ_OFFSET ; 4 ; p_flags byte 1 = RW <- it works without the executable bit set ?!
-;	xor al, al ; 2 ; eax is already zero at process start
+	xchg edx, ecx ; 2 ; p_flags RW
+	xchg eax, edx ; 1 ; zero eax
 	rep stosb ; 2
 	cld ; 1
 
