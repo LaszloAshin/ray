27d38b12e3d754bfd1feccdc2fa9f443cd8b84fe
github-action-benchmark <github@users.noreply.github.com>
2022-03-01 21:19:27 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 1a5e3ce23701f478842f11dd5a0ff03e68faf971

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1646140971737,
+  "lastUpdate": 1646169567788,
   "repoUrl": "https://github.com/LaszloAshin/ray",
   "entries": {
     "Benchmark": [
@@ -9043,6 +9043,65 @@ window.BENCHMARK_DATA = {
           {
             "name": "Benchmark - CPU Cycles",
             "value": 1010414987,
+            "unit": "Cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "laszlo@ashin.hu",
+            "name": "Laszlo Ashin",
+            "username": "LaszloAshin"
+          },
+          "committer": {
+            "email": "laszlo@ashin.hu",
+            "name": "Laszlo Ashin",
+            "username": "LaszloAshin"
+          },
+          "distinct": true,
+          "id": "1a5e3ce23701f478842f11dd5a0ff03e68faf971",
+          "message": "Move some large objects from stack to bss",
+          "timestamp": "2022-03-01T22:15:24+01:00",
+          "tree_id": "85d8eac2b4aa9a8606cbf6463e7533e8e953a5ce",
+          "url": "https://github.com/LaszloAshin/ray/commit/1a5e3ce23701f478842f11dd5a0ff03e68faf971"
+        },
+        "date": 1646169567081,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Benchmark - Executable Size",
+            "value": 3476,
+            "unit": "Bytes"
+          },
+          {
+            "name": "Benchmark - Win32 Executable Size",
+            "value": 3713,
+            "unit": "Bytes"
+          },
+          {
+            "name": "Benchmark - MacOS Executable Size",
+            "value": 8081,
+            "unit": "Bytes"
+          },
+          {
+            "name": "Benchmark - text Size",
+            "value": 5272,
+            "unit": "Bytes"
+          },
+          {
+            "name": "Benchmark - data Size",
+            "value": 0,
+            "unit": "Bytes"
+          },
+          {
+            "name": "Benchmark - bss Size",
+            "value": 336,
+            "unit": "Bytes"
+          },
+          {
+            "name": "Benchmark - CPU Cycles",
+            "value": 1010414986,
             "unit": "Cycles"
           }
         ]
